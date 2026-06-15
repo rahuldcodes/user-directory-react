2b191875b5878a2307f25c006926ef7d261e91eb
rahuldcodes <rahul453dubey@gmail.com>
2026-06-16 03:34:21 +0530

    Change import path for App component
    Updated import path for App component to use new structure.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './src/App' // Yeh ab seedhe tumhaare double src wale folder se App.jsx utha lega!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
