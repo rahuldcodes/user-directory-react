12682232f098c0535d64ae2708adf679e6531fa4
rahuldcodes <rahul453dubey@gmail.com>
2026-06-16 03:34:49 +0530

    Fix import path for App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/App' // Yeh ab seedhe tumhaare double src wale folder se App.jsx utha lega!
+import App from './src/App' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
